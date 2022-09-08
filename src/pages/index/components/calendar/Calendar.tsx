d26879e31112943d886eb9cb62d46d05e7e171ab
rookie996-object <472099249@qq.com>
2022-09-08 23:47:38 +0800

    feat: calendar style structure

--- src/pages/index/components/calendar/Calendar.tsx
+++ src/pages/index/components/calendar/Calendar.tsx
@@ -12,17 +12,12 @@ export default () => {
     const currentMonth: number = new Date().getMonth() + 1
 
     const weekOfEarlyMonth: number = new Date(currentYear, currentMonth -1, 1).getDay()
-
-    const weekOfEndMonth: number = new Date(currentYear, currentMonth -1, MONTH_DAYS[currentMonth - 1]).getDay()
     
-    const days: any[] = [];
+    const days: Array<number> = [];
     for (let i: number = 1; i <= 35; i++){
-      if(i > weekOfEarlyMonth){
-        days.push(i - weekOfEarlyMonth);
-      } else {
-        days.push(null);
-      }
+      i > weekOfEarlyMonth ? days.push(i - weekOfEarlyMonth) : days.push(0)
     }
+    days.map((item, index) => { if(item && item > MONTH_DAYS[currentMonth - 1]) days[index] = 0 })
 
     return (
         <View>
@@ -38,11 +33,14 @@ export default () => {
             <View className='table-body'>
               {
                 days.map((item, key) => {
-                  return <View className='table-body-content' key={key}>{item}</View>
+                  return <View className='table-body-content' key={key}>{item === 0 ? null : item}</View>
                 })
               }
             </View>
-        </View>
+          </View>
+          <View className='daily-attendance-button'>
+            <Text>已坚持300天</Text>
+          </View>
         </View>
       )
 }
