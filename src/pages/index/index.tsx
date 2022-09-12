e5f323de2f78d235b835748aea8bf7c2ba2e66e3
rookie996-object <472099249@qq.com>
2022-09-12 22:55:58 +0800

    style: code format

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,21 +1,20 @@
-import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
-import Calendar from './components/calendar/Calendar'
-import './index.scss'
+import { Component, PropsWithChildren } from "react";
+import { View, Text } from "@tarojs/components";
+import Calendar from "./components/calendar/Calendar";
+import "./index.scss";
 
 export default class Index extends Component<PropsWithChildren> {
+  componentWillMount() { }
 
-  componentWillMount () { }
+  componentDidMount() { }
 
-  componentDidMount () { }
+  componentWillUnmount() { }
 
-  componentWillUnmount () { }
+  componentDidShow() { }
 
-  componentDidShow () { }
+  componentDidHide() { }
 
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
       <View className='index'>
         <Calendar></Calendar>
