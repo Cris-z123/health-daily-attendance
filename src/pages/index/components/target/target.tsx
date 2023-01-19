ee0a3878132f4586e33e7cccc8aa31319b9b4965
rookie996-object <472099249@qq.com>
2023-01-19 22:03:56 +0800

    :bug: fix: perf task text

--- src/pages/index/components/target/target.tsx
+++ src/pages/index/components/target/target.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import './target.scss'
 
 const HealthItem = [
-  { id: '0000', content: '今天坚持低糖', status: 'FINISHED', },
-  { id: '1111', content: '今天运动半小时', status: 'FINISHED', },
-  { id: '2222', content: '今天喝了超过1.5L水', status: 'UNFINISHED', },
+  { id: '123456', content: '坚持低糖', status: 'FINISHED', },
+  { id: '234567', content: '运动半小时', status: 'FINISHED', },
+  { id: '345678', content: '喝水1.5L以上', status: 'UNFINISHED', },
 ]
 
 
